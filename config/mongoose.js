f2d13c1f6ac0727a2a3840a9e379911224cea6a4
Pratik Patharkar <pratik.21810592@viit.ac.in>
2022-07-25 18:02:46 +0530

    bug fixes

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@ main().catch(err => console.log(err));
 const env = require('./environment')  
  
 async function main() {
-  await mongoose.connect('mongodb://35.90.225.77:27017/codial_development')
+  await mongoose.connect('mongodb://localhost:27017/codial_development')
 }
 
 module.exports = main
